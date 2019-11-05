97f8e5500d1a521f1189ca66fdee97130412549e
Darrin Lowery <lambda@monsto.com>
2019-11-05 09:41:34 -0600

    complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import React, { useState } from 'react';
-import { Route } from 'react-router-dom';
-import data from './data';
+import React, { usecontext, useState } from "react";
+import { Route } from "react-router-dom";
+import data from "./data";
+
+import { ProductContext, ProductProvider } from "./contexts/ProductContext";
+import { CartContext } from "./contexts/CartContext";
 
 // Components
-import Navigation from './components/Navigation';
-import Products from './components/Products';
-import ShoppingCart from './components/ShoppingCart';
+import Navigation from "./components/Navigation";
+import Products from "./components/Products";
+import ShoppingCart from "./components/ShoppingCart";
+
+function clg(...x) {
+	for (let exes of x) console.log(exes);
+}
 
 function App() {
 	const [products] = useState(data);
@@ -13,28 +20,21 @@ function App() {
 
 	const addItem = item => {
 		// add the given item to the cart
+		clg("addItem");
+		setCart([...cart, item]);
 	};
 
 	return (
 		<div className="App">
-			<Navigation cart={cart} />
+			<ProductContext.Provider value={{ products, addItem }}>
+				<CartContext.Provider value={{ cart }}>
+					<Navigation cart={cart} />
 
-			{/* Routes */}
-			<Route
-				exact
-				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
-					/>
-				)}
-			/>
+					<Route exact path="/" component={Products} />
 
-			<Route
-				path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
-			/>
+					<Route exact path="/cart" component={ShoppingCart} />
+				</CartContext.Provider>
+			</ProductContext.Provider>
 		</div>
 	);
 }
---
+++ src/contexts/CartContext.js
@@ -0,0 +1,7 @@
+import React, {createContext} from 'react';
+
+export const CartContext = createContext()
+
+// export const CartProvider = (props) => {
+// 	const
+// }
